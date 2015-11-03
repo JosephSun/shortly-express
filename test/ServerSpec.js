c710a3d63fefd6fe5cbcd921e556deb2d288334a
JosephSun <bazzkhurshid@yahoo.com>
2015-11-02 19:50:12 -0800

    commented out the first 6 tests. Removed x for the 4 passing and four failing.

--- test/ServerSpec.js
+++ test/ServerSpec.js
@@ -68,166 +68,165 @@ describe('', function() {
       });
   });
 
-  describe('Link creation:', function(){
+  // describe('Link creation:', function(){
 
-    var requestWithSession = request.defaults({jar: true});
+  //   var requestWithSession = request.defaults({jar: true});
 
-    xbeforeEach(function(){
-      // create a user that we can then log-in with
-      new User({
-          'username': 'Phillip',
-          'password': 'Phillip'
-      }).save().then(function(){
-        var options = {
-          'method': 'POST',
-          'followAllRedirects': true,
-          'uri': 'http://127.0.0.1:4568/login',
-          'json': {
-            'username': 'Phillip',
-            'password': 'Phillip'
-          }
-        };
-        // login via form and save session info
-        requestWithSession(options, function(error, res, body) {
-          done();
-        });
-      });
-    });
+  //   xbeforeEach(function(){
+  //     // create a user that we can then log-in with
+  //     new User({
+  //         'username': 'Phillip',
+  //         'password': 'Phillip'
+  //     }).save().then(function(){
+  //       var options = {
+  //         'method': 'POST',
+  //         'followAllRedirects': true,
+  //         'uri': 'http://127.0.0.1:4568/login',
+  //         'json': {
+  //           'username': 'Phillip',
+  //           'password': 'Phillip'
+  //         }
+  //       };
+  //       // login via form and save session info
+  //       requestWithSession(options, function(error, res, body) {
+  //         done();
+  //       });
+  //     });
+  //   });
 
-    it('Only shortens valid urls, returning a 404 - Not found for invalid urls', function(done) {
-      var options = {
-        'method': 'POST',
-        'uri': 'http://127.0.0.1:4568/links',
-        'json': {
-          'url': 'definitely not a valid url'
-        }
-      };
+  //   it('Only shortens valid urls, returning a 404 - Not found for invalid urls', function(done) {
+  //     var options = {
+  //       'method': 'POST',
+  //       'uri': 'http://127.0.0.1:4568/links',
+  //       'json': {
+  //         'url': 'definitely not a valid url'
+  //       }
+  //     };
 
-      requestWithSession(options, function(error, res, body) {
-        // res comes from the request module, and may not follow express conventions
-        expect(res.statusCode).to.equal(404);
-        done();
-      });
-    });
+  //     requestWithSession(options, function(error, res, body) {
+  //       // res comes from the request module, and may not follow express conventions
+  //       expect(res.statusCode).to.equal(404);
+  //       done();
+  //     });
+  //   });
 
-    describe('Shortening links:', function(){
+  //   // describe('Shortening links:', function(){
 
-      var options = {
-        'method': 'POST',
-        'followAllRedirects': true,
-        'uri': 'http://127.0.0.1:4568/links',
-        'json': {
-          'url': 'http://www.github.com/'
-        }
-      };
+  //   //   var options = {
+  //   //     'method': 'POST',
+  //   //     'followAllRedirects': true,
+  //   //     'uri': 'http://127.0.0.1:4568/links',
+  //   //     'json': {
+  //   //       'url': 'http://www.github.com/'
+  //   //     }
+  //   //   };
 
-      it('Responds with the short code', function(done) {
-        requestWithSession(options, function(error, res, body) {
-          expect(res.body.url).to.equal('http://www.github.com/');
-          expect(res.body.code).to.not.be.null;
-          done();
-        });
-      });
+  //   //   it('Responds with the short code', function(done) {
+  //   //     requestWithSession(options, function(error, res, body) {
+  //   //       expect(res.body.url).to.equal('http://www.github.com/');
+  //   //       expect(res.body.code).to.not.be.null;
+  //   //       done();
+  //   //     });
+  //   //   });
 
-      it('New links create a database entry', function(done) {
-        requestWithSession(options, function(error, res, body) {
-          db.knex('urls')
-            .where('url', '=', 'http://www.github.com/')
-            .then(function(urls) {
-              if (urls['0'] && urls['0']['url']) {
-                var foundUrl = urls['0']['url'];
-              }
-              // console.log("FoundUrl", foundUrl);
-              expect(foundUrl).to.equal('http://www.github.com/');
-              done();
-            });
-        });
-      });
+  //   //   it('New links create a database entry', function(done) {
+  //   //     requestWithSession(options, function(error, res, body) {
+  //   //       db.knex('urls')
+  //   //         .where('url', '=', 'http://www.github.com/')
+  //   //         .then(function(urls) {
+  //   //           if (urls['0'] && urls['0']['url']) {
+  //   //             var foundUrl = urls['0']['url'];
+  //   //           }
+  //   //           // console.log("FoundUrl", foundUrl);
+  //   //           expect(foundUrl).to.equal('http://www.github.com/');
+  //   //           done();
+  //   //         });
+  //   //     });
+  //   //   });
 
-      it('Fetches the link url title', function (done) {
-        requestWithSession(options, function(error, res, body) {
-          db.knex('urls')
-            .where('title', '=', 'GitHub · Where software is built')
-            .then(function(urls) {
-              if (urls['0'] && urls['0']['title']) {
-                var foundTitle = urls['0']['title'];
-              }
-              expect(foundTitle).to.equal('GitHub · Where software is built');
-              done();
-            });
-        });
-      });
+  //   //   it('Fetches the link url title', function (done) {
+  //   //     requestWithSession(options, function(error, res, body) {
+  //   //       db.knex('urls')
+  //   //         .where('title', '=', 'GitHub · Where software is built')
+  //   //         .then(function(urls) {
+  //   //           if (urls['0'] && urls['0']['title']) {
+  //   //             var foundTitle = urls['0']['title'];
+  //   //           }
+  //   //           expect(foundTitle).to.equal('GitHub · Where software is built');
+  //   //           done();
+  //   //         });
+  //   //     });
+  //   //   });
 
-    }); // 'Shortening links'
+  //   // }); // 'Shortening links'
 
-    describe('With previously saved urls:', function(){
+  //   describe('With previously saved urls:', function(){
 
-      var link;
+  //     var link;
 
-      beforeEach(function(done){
-        // save a link to the database
-        link = new Link({
-          url: 'http://www.github.com/',
-          title: 'GitHub · Where software is built',
-          base_url: 'http://127.0.0.1:4568'
-        });
-        link.save().then(function(){
-          done();
-        });
-      });
+  //     beforeEach(function(done){
+  //       // save a link to the database
+  //       link = new Link({
+  //         url: 'http://www.github.com/',
+  //         title: 'GitHub · Where software is built',
+  //         base_url: 'http://127.0.0.1:4568'
+  //       });
+  //       link.save().then(function(){
+  //         done();
+  //       });
+  //     });
 
-      it('Returns the same shortened code', function(done) {
-        var options = {
-          'method': 'POST',
-          'followAllRedirects': true,
-          'uri': 'http://127.0.0.1:4568/links',
-          'json': {
-            'url': 'http://www.github.com/'
-          }
-        };
+  //     it('Returns the same shortened code', function(done) {
+  //       var options = {
+  //         'method': 'POST',
+  //         'followAllRedirects': true,
+  //         'uri': 'http://127.0.0.1:4568/links',
+  //         'json': {
+  //           'url': 'http://www.github.com/'
+  //         }
+  //       };
 
-        requestWithSession(options, function(error, res, body) {
-          var code = res.body.code;
-          expect(code).to.equal(link.get('code'));
-          done();
-        });
-      });
+  //       requestWithSession(options, function(error, res, body) {
+  //         var code = res.body.code;
+  //         expect(code).to.equal(link.get('code'));
+  //         done();
+  //       });
+  //     });
 
-      it('Shortcode redirects to correct url', function(done) {
-        var options = {
-          'method': 'GET',
-          'uri': 'http://127.0.0.1:4568/' + link.get('code')
-        };
+  //     it('Shortcode redirects to correct url', function(done) {
+  //       var options = {
+  //         'method': 'GET',
+  //         'uri': 'http://127.0.0.1:4568/' + link.get('code')
+  //       };
 
-        requestWithSession(options, function(error, res, body) {
-          var currentLocation = res.request.href;
-          expect(currentLocation).to.equal('https://github.com/');
-          done();
-        });
-      });
+  //       requestWithSession(options, function(error, res, body) {
+  //         var currentLocation = res.request.href;
+  //         expect(currentLocation).to.equal('https://github.com/');
+  //         done();
+  //       });
+  //     });
 
-      it('Returns all of the links to display on the links page', function(done) {
-        var options = {
-          'method': 'GET',
-          'uri': 'http://127.0.0.1:4568/links'
-        };
+  //     it('Returns all of the links to display on the links page', function(done) {
+  //       var options = {
+  //         'method': 'GET',
+  //         'uri': 'http://127.0.0.1:4568/links'
+  //       };
 
-        requestWithSession(options, function(error, res, body) {
-          expect(body).to.include('"title":"GitHub · Where software is built"');
-          expect(body).to.include('"code":"' + link.get('code') + '"');
-          done();
-        });
-      });
+  //       requestWithSession(options, function(error, res, body) {
+  //         expect(body).to.include('"title":"GitHub · Where software is built"');
+  //         expect(body).to.include('"code":"' + link.get('code') + '"');
+  //         done();
+  //       });
+  //     });
 
-    }); // 'With previously saved urls'
+  //   }); // 'With previously saved urls'
 
-  }); // 'Link creation'
+  // }); // 'Link creation'
 
   describe('Privileged Access:', function(){
 
     it('Redirects to login page if a user tries to access the main page and is not signed in', function(done) {
       request('http://127.0.0.1:4568/', function(error, res, body) {
-        console.log("res", res, "body", body, "************************************************************************************************************");
         expect(res.req.path).to.equal('/login');
         done();
       });
@@ -249,7 +248,7 @@ describe('', function() {
 
   }); // 'Priviledged Access'
 
-  xdescribe('Account Creation:', function(){
+  describe('Account Creation:', function(){
 
     it('Signup creates a user record', function(done) {
       var options = {
@@ -297,7 +296,7 @@ describe('', function() {
 
   }); // 'Account Creation'
 
-  xdescribe('Account Login:', function(){
+  describe('Account Login:', function(){
 
     var requestWithSession = request.defaults({jar: true});
 
